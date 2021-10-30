3ca09b90101d3cca61a5ce20999a66f7fdd32af7
Priyanshu Nagde <66083653+pnagde@users.noreply.github.com>
2021-10-30 10:47:14 +0530

    Create 217. Contains Duplicate.java

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,35 @@
+//Time Complexity = O(n)
+//Space Complexity= O(n)
+//Question- Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+/*
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n=nums.length;
+        boolean present=false;
+        HashMap<Integer,Integer> hmap=new HashMap<>();
+        for(int i=0;i<n;++i){
+            if(hmap.containsKey(nums[i]))
+            {
+                present=true;
+                break;
+            }else{
+                hmap.put(nums[i],1);
+            }
+        }
+        
+        return present;
+    }
+}
